a209e657c3aab9f272cab3ff07d02b3c18a78c70
arifraj199 <karif9514@gmail.com>
2022-05-19 02:44:40 +0600

    home page implement

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img src="https://i.ibb.co/2kP6cVG/491.jpg" className='w-96' alt='to-do img' />
+          <div>
+            <h1 class="text-5xl font-bold">Simple To-do List</h1>
+            <p class="py-6">Note down of your daily work and keep important list safe.</p>
+            <button class="btn btn-primary">Add Task</button>
+          </div>
         </div>
+      </div>
     );
 };
 
