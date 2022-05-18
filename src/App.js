e96afe8d0cd2eba91455d0faccd07da7d50434a4
arifraj199 <karif9514@gmail.com>
2022-05-19 02:29:35 +0600

    Route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddTask from './components/AddTask';
+import Home from './components/Home';
+import Login from './components/Login';
+import MyTask from './components/MyTask';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to Simple Todo List</h2>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/addtask' element={<AddTask></AddTask>}></Route>
+        <Route path='/mytask' element={<MyTask></MyTask>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+      </Routes>
     </div>
   );
 }
